e307342b694bf1744fd2611a9bd7bd824c8ecd54
SantaCruz77 <steveyamada66@gmail.com>
2023-01-31 16:26:37 +0900

    ショップ情報

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -60,7 +60,7 @@
                             <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">{{ $product->category->name }}</h2>
                             <h1 class="mb-4 text-gray-900 text-3xl title-font font-medium">{{ $product->name }}</h1>
                             <p class="mb-4 leading-relaxed">{{ $product->information }}</p>
-                            <div class="flex justify-around items-center">
+                            <div class="mb-4 flex justify-around items-center">
                                 <div>
                                     <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm text-gray-700">円(税込)</span>
                                 </div>
@@ -79,9 +79,44 @@
                             </div>
                         </div>
                     </div>
+                    <div class="border-t border-gray-400 my-8"></div>
+                    <div class="mb-4 text-center">この商品を販売しているショップ</div>
+                    <div class="mb-4 text-center">{{ $product->shop->name }}</div>
+                    <div class="mb-4 text-center">
+                        @if ($product->shop->filename !== null)
+                            <img class="mx-auto w-40 h-40 object-cover rounded-full" src="{{ asset('storage/shops/' . $product->shop->filename )}}">
+                        @else
+                            <img src="">
+                        @endif
+                    </div>
+                    <div class="mb-4 text-center">
+                        <button data-micromodal-trigger="modal-1" href='javascript:;' type="button" class="text-white bg-gray-400 border-0 py-2 px-6 focus:outline-none hover:bg-gray-500 rounded">ショップの詳細を見る</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+        <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+            <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+                <header class="modal__header">
+                    <h2 class="text-xl text-gray700" id="modal-1-title">
+                        {{ $product->shop->name }}
+                    </h2>
+                    <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+                </header>
+                <main class="modal__content" id="modal-1-content">
+                    <p>
+                        {{ $product->shop->information }}
+                    </p>
+                </main>
+                <footer class="modal__footer">
+                    <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
+                </footer>
+            </div>
+        </div>
+      </div>
+
     <script src="{{ mix('js/swiper.js') }}"></script>
 </x-app-layout>
