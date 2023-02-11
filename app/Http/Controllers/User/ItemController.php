0bdd5087777386ef415172f7ddac1051e36bb10c
SantaCruz77 <steveyamada66@gmail.com>
2023-02-11 15:01:31 +0900

    ユーザー向けメール送信

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,7 +10,6 @@ use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
-use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -40,7 +39,7 @@ class ItemController extends Controller
         // ->send(new TestMail());
 
         // 非同期に送信
-        SendThanksMail::dispatch();
+        // SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
